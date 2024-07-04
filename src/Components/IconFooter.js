2dac0c56acc271febc3026ca8be70f8c493ca0d5
Saloni Sharma <salonisharma9700@gmail.com>
2024-07-04 09:02:55 +0530

    2.0

--- src/Components/IconFooter.js
+++ src/Components/IconFooter.js
@@ -10,30 +10,32 @@ const IconFooter = () => {
     window.scrollTo(0, 0);
   }, [location]);
   return (
-    <div className="footer-row footer-bottom-container footer-justify-content-center">
-      <div className="footer-col-3 footer-col-sm-2">
-        <Link to="/ytvid" className="footer-link">
-          <strong>Know More</strong>
-          <img src="/vidicon.png" className="footer-icons" alt="About Icon" /><br />
-        </Link>
-      </div>
-      <div className="footer-col-3 footer-col-sm-2">
-        <Link to="/upvid" className="footer-link">
-          <strong>Upload Video</strong>
-          <img src="images/upload.png" className="footer-icons" alt="Home Icon" /><br />
-        </Link>
-      </div>
-      <div className="footer-col-3 footer-col-sm-2">
-        <Link to="/faq" className="footer-link">
-          <strong>FAQ </strong>
-          <img src="images/faq.png" className="footer-icons" alt="About Icon" /><br />
-        </Link>
-      </div>
-      <div className="footer-col-3 footer-col-sm-2">
-        <Link to="/contact" className="footer-link">
-          <strong>Contact Us</strong>
-          <img src="images/contact-us.png" className="footer-icons" alt="Home Icon" /><br />
-        </Link>
+    <div className="icon-grid">
+      <div className="row text-center">
+        <div className="col-6 col-md-3 mb-4">
+          <Link to="/ytvid" className="footer-link d-flex flex-column align-items-center">
+            <strong>Know More</strong>
+            <img src="/vidicon.png" className="footer-icons mt-2" alt="About Icon" />
+          </Link>
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <Link to="/faq" className="footer-link d-flex flex-column align-items-center">
+            <strong>FAQ</strong>
+            <img src="images/faq.png" className="footer-icons mt-2" alt="FAQ Icon" />
+          </Link>
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <Link to="/upvid" className="footer-link d-flex flex-column align-items-center">
+            <strong>Upload Video</strong>
+            <img src="images/upload.png" className="footer-icons mt-2" alt="Upload Icon" />
+          </Link>
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <Link to="/contact" className="footer-link d-flex flex-column align-items-center">
+            <strong>Contact Us</strong>
+            <img src="images/contact-us.png" className="footer-icons mt-2" alt="Contact Icon" />
+          </Link>
+        </div>
       </div>
     </div>
   );
